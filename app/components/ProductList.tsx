62eac22836b709281afe1c4efb0690e4f05478de
norainiibrahim <norainiibrahim98@gmail.com>
2024-05-23 08:47:10 +0800

    refactor: [WIP] Comment out dynamic link

--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -1,9 +1,9 @@
 'use client';
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import AddProductForm from './AddProductForm';
-import Image from 'next/image';
 import Link from 'next/link';
+import AddProductForm from './AddProductForm';
 
 interface Product {
   id: number;
@@ -24,7 +24,6 @@ const ProductList: React.FC = () => {
         console.error('Error fetching products:', error);
       }
     };
-
     fetchProducts();
   }, []);
 
@@ -38,13 +37,13 @@ const ProductList: React.FC = () => {
       <h1 className="text-2xl font-bold mb-4 mt-12">Product List</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {products.map((product) => (
-          <Link key={product.id} href={`/product/${product.id}`}>
-              <div className="bg-white shadow-md rounded-lg p-4">
-                <img src={product.image} alt={product.name} className="w-full h-40 object-cover mb-2" />
-                <h2 className="text-lg font-semibold text-black">{product.name}</h2>
-                <p className="text-gray-500">RM{product.price.toFixed(2)}</p>
-              </div>
-          </Link>
+          // <Link  href={`/product/${product.id}`}>
+            <div key={product.id} className="bg-white shadow-md rounded-lg p-4">
+              <img src={product.image} alt={product.name} className="w-full h-40 object-cover mb-2" />
+              <h2 className="text-lg font-semibold text-black">{product.name}</h2>
+              <p className="text-gray-500">RM{product.price.toFixed(2)}</p>
+            </div>
+          // </Link>
         ))}
       </div>
     </div>
